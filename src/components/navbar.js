f6501dd4d30ac9b973af7b6b990b64c69979491d
King-Dorji <kingoridenis74@gmail.com>
2023-03-01 12:53:35 +0300

    initial react project

---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import {
+  menuItems
+} from "./menuItems";
+import MenuItems from "./MenuItemsControl";
+const Navbar = () => {
+  return ( 
+  <nav>
+      <ul className = "menus">
+        {
+          menuItems.map((menu, index) => {
+              const depthLevel = 0;
+              return <MenuItems items = {
+                  menu
+              }
+              key = {
+                  index
+              }
+              depthLevel = {
+                  depthLevel
+              }
+              />;
+          })
+        } 
+      </ul> 
+    </nav>
+  );
+};
+
+export default Navbar;
